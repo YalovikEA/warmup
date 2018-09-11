1680750e65b70d642aedbbf799f23156c8ea532e
Evgeny Yalovik <yalovikea@gmail.com>
2018-09-11 11:19:36 +0300

    warmup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+  //Для перевода температуры из шкалы Цельсия в шкалу Фаренгейта нужно умножить исходное число на 9/5 и прибавить 32.
+  return temperature * (9 / 5) + 32;
 };
